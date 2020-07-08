b748536607f0b5b875c1f34a37bfa05a00122806
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-07-08 20:57:57 +0200

    Implement dummy interfaces

---
+++ Dependencies/capstone/src/capstone.cpp
@@ -0,0 +1,30 @@
+#include <capstone/capstone.h>
+
+CAPSTONE_EXPORT cs_err CAPSTONE_API cs_open(cs_arch arch, cs_mode mode, csh* handle)
+{
+	return cs_err::CS_ERR_OK;
+}
+
+CAPSTONE_EXPORT cs_err CAPSTONE_API cs_option(csh handle, cs_opt_type type, size_t value)
+{
+	return cs_err::CS_ERR_OK;
+}
+
+CAPSTONE_EXPORT
+size_t CAPSTONE_API cs_disasm(csh handle,
+	const uint8_t* code, size_t code_size,
+	uint64_t address,
+	size_t count,
+	cs_insn** insn)
+{
+	return 0;
+}
+
+void CAPSTONE_API cs_free(cs_insn* insn, size_t count)
+{
+}
+
+const char* CAPSTONE_API cs_reg_name(csh handle, unsigned int reg_id)
+{
+	return 0;
+}
---
+++ Dependencies/keystone/src/keystone.cpp
@@ -0,0 +1,15 @@
+#include "..\include\keystone\keystone.h"
+
+KEYSTONE_EXPORT ks_err ks_open(ks_arch arch, int mode, ks_engine** ks)
+{
+	return ks_err::KS_ERR_OK;
+}
+
+int ks_asm(ks_engine* ks, const char* string, uint64_t address, unsigned char** encoding, size_t* encoding_size, size_t* stat_count)
+{
+	return 0;
+}
+
+void ks_free(unsigned char* p)
+{
+}
