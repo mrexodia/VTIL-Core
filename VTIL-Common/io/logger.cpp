e0fe69acbc96f46416c244415fa575be94a4704b
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-12-04 02:52:45 +0100

    Fix warnings for _CRT_SECURE_NO_WARNINGS when compiling with CMake

--- VTIL-Common/io/logger.cpp
+++ VTIL-Common/io/logger.cpp
@@ -26,9 +26,15 @@
 // POSSIBILITY OF SUCH DAMAGE.        
 //
 #if _WIN64
+    #ifndef _CRT_SECURE_NO_WARNINGS
 	#define _CRT_SECURE_NO_WARNINGS
+    #endif
+    #ifndef WIN32_LEAN_AND_MEAN
 	#define WIN32_LEAN_AND_MEAN
+    #endif
+    #ifndef NOMINMAX
 	#define NOMINMAX
+    #endif
 	#include <Windows.h>
 #endif
 #include "logger.hpp"
